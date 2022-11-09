36721cb0099cad903ce0549fc4dfa52e55be4c0c
Asher <60705763+AEBanthem@users.noreply.github.com>
2022-11-08 22:11:57 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,44 @@
-/**
- * This Main class is used to test JavaFX.
- */
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.input.MouseEvent;
-import javafx.application.Application;
+package src;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-//Alex was here too
-public class Main extends Application
-{
-    @Override
-    public void start(Stage stage) {   
-         
-    	CustomerGUI gui = new CustomerGUI();
-    	
-    	StackPane rootPane = new StackPane();
-        rootPane.getChildren().add(gui);
-        
-        //Creating a scene object 
-        Scene scene = new Scene(rootPane, 630, 540);  
-        
-        //Setting title to the Stage 
-        stage.setTitle("Pizza GUI"); 
-           
-        //Adding scene to the stage 
-        stage.setScene(scene); 
-           
-        //Displaying the contents of the stage 
-        stage.show(); 
-     }//end start
 
-    public static void main(String[] args)
-    {
-        launch(args);
-    }//end main
-    //changes
+public class CookGUI extends BorderPane{
     
+    private Label title, orderNum;
+    private Button cookButton;
+    private Button customerButton;
+    private VBox outer;
     
-    
+    public CookGUI(Order [] orders) {
+        outer = new VBox();
+        
+        title = new Label("Order");
+        outer.getChildren().addAll(title);
+        for(int i = 0; i < orders.length; i++)
+        {
+            Order order = orders[i];
+            orderNum = new Label("Order number" + order.getID());
+            outer.getChildren().addAll(orderNum,order);
+        }      
+        
+        
+        
+        Insets inset = new Insets(25);
+        outer.setPadding(inset);                
+        outer.setMargin(outer, new Insets(0, 0, 0, 8));
+        
+        this.setCenter(outer);
+    }
+
 }
