7f77f16edefd4b552cde39658189a0c09089b040
Slayer7075790 <117760552+Slayer7075790@users.noreply.github.com>
2022-11-10 16:22:36 -0700

    Name and ID input

--- src/CustomerGUI.java
+++ src/CustomerGUI.java
@@ -1,3 +1,5 @@
+package src;
+
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -13,6 +15,8 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.control.TextField;
+import java.util.Arrays;
 
 public class CustomerGUI extends BorderPane{
     
@@ -41,6 +45,12 @@ public class CustomerGUI extends BorderPane{
     private VBox vBox2;
     private VBox vBox3;
     private VBox vBox4;
+    //for the user input
+    private VBox vBox5;
+    
+    //create textboxes
+    private TextField nameTextField;
+    private TextField idTextField;
     
     private Button button1;
     
@@ -54,6 +64,7 @@ public class CustomerGUI extends BorderPane{
         label2 = new Label("Pizza Types");
         label3 = new Label("Pizza Toppings");
         label4 = new Label("Pizza Size");
+        
         
         /**
         pizza = new Image("pizza.png");
@@ -96,6 +107,16 @@ public class CustomerGUI extends BorderPane{
         checkBox3 = new CheckBox("Olives");
         checkBox4 = new CheckBox("Extra Cheese");
         
+        //create textboxs for name of order
+        Label nameBoxLabel = new Label("Name for Order:");
+        TextField nameTextField = new TextField();
+        nameTextField.setPromptText("John Doe");
+
+        Label idBoxLabel = new Label("ASU ID for Order:");
+        TextField idTextField = new TextField();
+        idTextField.setPromptText("0101010101");
+
+        
         //Creating Buttons 
         button1 = new Button("Add to Order"); 
 
@@ -109,6 +130,10 @@ public class CustomerGUI extends BorderPane{
         
         vBox4 = new VBox();
         
+        vBox5 = new VBox(); //user input
+        //Hbox to collect name for order
+        HBox nameHB = new HBox();
+        HBox idHB = new HBox();
         
         //add all the components to the vboxes
         vBox2.getChildren().add(rb1);
@@ -131,9 +156,18 @@ public class CustomerGUI extends BorderPane{
         vBox1.getChildren().add(vBox3);
         vBox1.getChildren().add(label4);
         vBox1.getChildren().add(vBox4);
+        vBox1.getChildren().add(vBox5);
         vBox1.getChildren().add(button1);
+        
         //vBox1.getChildren().add(pizzaWindow);
-
+        
+        vBox5.getChildren().add(nameHB);
+        vBox5.getChildren().add(idHB);
+        nameHB.getChildren().addAll(nameBoxLabel, nameTextField);
+        nameHB.setSpacing(10);
+        idHB.getChildren().addAll(idBoxLabel, idTextField);
+        idHB.setSpacing(10);
+        
         Insets inset = new Insets(25);
         vBox1.setPadding(inset);
         
@@ -143,15 +177,17 @@ public class CustomerGUI extends BorderPane{
         
         vBox4.setPadding(inset);
         
+        vBox5.setPadding(inset);
+        
         VBox.setMargin(vBox1, new Insets(0, 0, 0, 8));
         
         vBox1.setStyle("-fx-border-color: black");
         vBox2.setStyle("-fx-border-color: black");
         vBox3.setStyle("-fx-border-color: black");
         vBox4.setStyle("-fx-border-color: black");
+        vBox5.setStyle("-fx-border-color: black");
         
         this.setCenter(vBox1);
-        
         button1.setOnAction(new ButtonHandler());
     }
     
@@ -202,9 +238,63 @@ public class CustomerGUI extends BorderPane{
             if(checkBox4.isSelected()) {
                 toppings[3] = true;
             }
+                        
+            //get the id string and check if its all integers
+            boolean validOrder = true;
+            boolean validId = true;
+            boolean discount = false;
+            /*
+            System.out.println("PRINT STATEMENT BEFORE THINGS CRASH");
+            //name input
+            String name = nameTextField.getText();
+            String firstName = "";
+            String lastName = "";
+            char[] nameChars = name.toCharArray();
+            //loop through the name input to isolate the first and last names
+            for(int i = 0; i<nameChars.length;i++){
+                if (nameChars[i] == ' '){
+                    //seperate the first name
+                    char[] firstNameChar = Arrays.copyOfRange(nameChars,0,i-1);
+                    firstName = String.valueOf(firstNameChar);
+                    System.out.println(firstName);
+                    //seperate the last name
+                    char[] lastNameChar = Arrays.copyOfRange(nameChars,i+1,nameChars.length);
+                    lastName = String.valueOf(lastNameChar);
+                    System.out.println(lastName);
+                }
+            }
             
-            Pizza pizza = new Pizza(pizzaType, size, toppings);
+            //Id check
+            
+            String idString = idTextField.getText();
+            char[] idChars = idString.toCharArray();
+            for(int i = 0; i<idChars.length;i++){
+                if ((idChars[i] != 0)&&(idChars[i] != 1)&&(idChars[i] != 2)&&
+                    (idChars[i] != 3)&&(idChars[i] != 4)&&(idChars[i] != 5)&&
+                    (idChars[i] != 6)&&(idChars[i] != 7)&&(idChars[i] != 8)&&
+                    (idChars[i] != 9)){
+                    validId = false;
+                }
+                if (i > 9){
+                    validId = false;
+                }
+            }
+            //parse the id input to a int
+            int id = 0;
+            if (validId){
+                id =  Integer.parseInt(idTextField.getText());
+            }
+            System.out.println(id);
+            */
+            
+            if (validId){
+                discount = true;
+                Order newOrder = new Order(pizzaType, size, toppings, /*id*/0000000000, /*firstName*/"firstName", /*lastName*/"lastName", discount);
+            } else{
+                Order newOrder = new Order(pizzaType, size, toppings, 0000000000, /*firstName*/"firstName", /*lastName*/"lastName", discount);
+            }
 
+            Pizza pizza = new Pizza(pizzaType, size, toppings);
             
             System.out.println();
             
